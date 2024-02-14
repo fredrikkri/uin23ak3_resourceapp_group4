9a96425af54350a555f3f078b7704088e32aa0d1
fredrikkri <fjkri@hotmail.com>
2024-02-14 09:40:17 +0100

    begynte smått med å prøve å lage starten av react prosjektet

--- Ressursarkiv/src/App.jsx
+++ Ressursarkiv/src/App.jsx
@@ -4,7 +4,16 @@ function App() {
 
   return (
     <>
-      
+      <body>
+        <header>
+            <nav>
+              <ul></ul>
+            </nav>
+        </header>
+        <main></main>
+        <script src="js/ressurser.js"></script>
+        <script src="js/script.js"></script>
+      </body>
     </>
   )
 }
